7624e81a2a204b5c0050d9c18ea341b61f1f2e79
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2023-10-10 22:13:25 -0400

    Find posts

--- services/dashboard_service.py
+++ services/dashboard_service.py
@@ -1,8 +1,10 @@
 from models.mongo.post import Post
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 class DashboardService:
 
     @classmethod
     async def get_posts_by_user(cls, user):
-        posts = await Post.find(user=user)
+        posts = await Post.find(Post.user.id==user.id).to_list()
         return posts
