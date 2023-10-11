69397763e72866ce5c940058b98db73fb3e91409
arunkls <arunkls195@gmail.com>
2023-10-11 00:00:57 -0400

    dashboard service

--- api/dashboard.py
+++ api/dashboard.py
@@ -4,16 +4,18 @@ from services.dashboard_service import DashboardService
 from services.neo4j_service import Neo4jService
 from services.user_service import get_current_user
 from models.mongo.user import User
-
 router = APIRouter()
 
 
 @router.get('/dashboard-posts')
 async def dashboard_posts(user: Annotated[User, Depends(get_current_user)]):
     try:
-        # connections = await Neo4jService.all_connections(user)
-        # serialized_connections= await Neo4jService.serialize(connections)
-        posts = await DashboardService.get_posts_by_user(user)
+        connections = await Neo4jService.all_connections(user)
+        users = [user]
+        for connection in connections:
+            users.append(await Neo4jService.graph_user_to_user(connection))
+        posts = await DashboardService.get_posts_by_users(users)
+
         return {"status": 200, "message": "Fetch Successful", "posts": posts}
     except Exception as e:
         return {"status": 500, "message": f"Failed with error: {e}"}
--- services/dashboard_service.py
+++ services/dashboard_service.py
@@ -1,10 +1,18 @@
 from models.mongo.post import Post
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
+
 
 class DashboardService:
 
     @classmethod
     async def get_posts_by_user(cls, user):
-        posts = await Post.find(Post.user.id==user.id).to_list()
+        posts = await Post.find(Post.user.id == user.id, fetch_links=True).to_list()
         return posts
+    
+    @classmethod
+    async def get_posts_by_users(cls, users):
+        posts = []
+        for user in users:
+            print(user.email)
+            posts.extend(await cls.get_posts_by_user(user))
+        return posts
+
